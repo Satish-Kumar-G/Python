8878e8af7a2d3adf93a2c27728f5bc182bfb1e2e
Satish Kumar G <garthimas@gmail.com>
2019-08-18 19:56:29 +0530

    Create nthsmallest_bubblesort.py

---
+++ nthsmallest_bubblesort.py
@@ -0,0 +1,10 @@
+# N th Smallest using bubble sort
+
+#Bubble sort Technique
+arr=list(map(int,input().split()))
+N=int(input())
+for i in range(N):
+	for j in range(i,len(arr)):
+		if(arr[i]>arr[j]):
+			arr[i],arr[j]=arr[j],arr[i]
+print(arr[N-1])
