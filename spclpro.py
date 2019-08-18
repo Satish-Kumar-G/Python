b6006e235ad1a44a481d9ced54b73731ca9bd3c0
Satish Kumar G <garthimas@gmail.com>
2019-08-18 19:25:24 +0530

    Update spclpro.py

--- spclpro.py
+++ spclpro.py
@@ -3,10 +3,17 @@
 #Driver code
 n=int(input())
 inp=list(map(int,input().split()))
+
+omly this code
+
 pro=1
 out=[]
 for i in inp:
 	pro*=int(i)
 for i in inp:
 	out.append(pro//int(i))
+
+
+till here
+
 print(out)
